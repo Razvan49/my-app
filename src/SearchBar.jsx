f41adfa1c81458d1c1ab3e833ff1e66fa09a2c5c
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-12-06 14:51:04 +0200

    My first React project

---
+++ src/SearchBar.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+function SearchBar({ isVisible, text, price }) {
+  const helpText = text.length > 10 && <p>Textul este mai lung de zece caractere</p>;
+  return (
+    <div>
+      {isVisible ? "Pentru ca este vizibil" : ""}
+      {helpText}
+      <h1>{text}</h1>Search
+      <h2>{price}</h2>
+    </div>
+  );
+}
+
+SearchBar.propTypes = {
+  isVisible: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default SearchBar;
